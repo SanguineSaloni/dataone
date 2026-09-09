3538d4bb321e5c9f0dc5523957453aa00c075dfc
Vaibhav Vijay <vaibhav@Vaibhavs-MacBook-Air.local>
2026-09-09 09:42:25 +0530

    Remove invalid experimental config property

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -8,10 +8,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // Ensure all pages are statically exported
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
 };
 
 export default nextConfig;
